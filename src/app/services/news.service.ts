714956737cba5bb382846075cbaefa766e9ba0d8
Dulce López <dulhelengi@gmail.com>
2023-10-05 13:32:20 -0600

    Evaluación

---
+++ src/app/services/news.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient} from '@angular/common/http';
+import { Injectable} from '@angular/core';
+import { environment} from 'src/environments/environment';
+
+import { NewsResponse,Article} from '../interfaces'
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+const apikey = environment.apiKey
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsService {
+
+  constructor(private http: HttpClient) { }
+
+getTopHeadlines():Observable<Article[]>{
+  return this.http.get<NewsResponse>('https://newsapi.org/v2/top-headlines?country=us&category=business', { params:{apikey}})
+  .pipe(map(({articles}) =>articles))
+}
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiKey: '0e9212c4127c45a5bd84a24247b2a45b'
+};
